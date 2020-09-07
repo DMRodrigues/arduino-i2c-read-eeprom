5a238915a8827c8ed9518f7d7a5ee1b6e362e598
DMRodrigues <diogo.miguel.rodrigues@ist.utl.pt>
2020-09-07 12:48:47 +0100

    made C89 compliant

--- i2c_read_eeprom.c
+++ i2c_read_eeprom.c
@@ -68,6 +68,26 @@ int to_termios_baudrate(unsigned int baudrate)
 	}
 }
 
+static void print_eeprom()
+{
+	int i;
+	printf("\n-----\nRead the datasheet of the EEPROM and identify the addressing type compatible with one implemented.\n");
+	printf("\nDefault, EEPROMs bigger than 4Kbytes(32 Kbits)");
+	printf("\n1 | 0 | 1 | 0 | A2 | A1/B1/P1/A17 | A0/P0/B0/A16\n");
+	printf("	Devices:\n		");	
+	for(i = 0; type[i]; i++)
+		printf("%s, ", type[i]);
+	printf("\n\nEEPROMs up to 2Kbytes(16Kbits)\n1 | 0 | 1 | 0 | A2/B2/P2/A10 | A1/B1/P1/A9 | A0/B0/P0/A8  =>  -d 1\n");
+	printf("	Devices:\n		");
+	for(i = 0; type1[i]; i++)
+		printf("%s,", type1[i]);
+	printf("\n\nOther type of addressing\n1 | 0 | 1 | 0 | P0/B0/A16 | A1 | A0  =>  -d 2\n");
+	printf("	Devices:\n		");
+	for(i = 0; type2[i]; i++)
+		printf("%s,", type2[i]);
+	printf("\n-----");
+}
+
 static void print_usage(const char *program_name, int flag_print_eeprom)
 {
 	printf("\nUsage: %s [OPTIONS...]\n", program_name);
